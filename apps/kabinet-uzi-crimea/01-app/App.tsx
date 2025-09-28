840a397044c2430782f309e276265f618a85c1df
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-09-28 15:28:18 +0500

    fix(): dir apps -> sub

--- apps/kabinet-uzi-crimea/01-app/App.tsx
+++
@@ -1,3 +0,0 @@
-export const UziApp = () => {
-  return <>UZI</>;
-};
---
+++ sub/kabinet-uzi-crimea/01-app/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '#ui/button';
+
+export const UziApp = () => {
+  return (
+    <>
+      <Button
+        // class="bg-red-300  "
+        variant={'default'}
+        size={'icon'}
+        onClick={console.log}
+      >
+        asdasd
+      </Button>
+    </>
+  );
+};
